1d922b5a1cbb8ffc34f260103c59701e0b044444
carraes <carraeshb@gmail.com>
2022-11-07 11:38:22 -0300

    build: add search controllers

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,13 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { featured, name, company } = req.query
+  const queryObject = {}
+  if (featured) queryObject.featured = featured === 'true'
+  if (company) queryObject.company = company
+  if (name) queryObject.name = { $regex: name, $options: 'i' }
+  console.log(queryObject)
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
